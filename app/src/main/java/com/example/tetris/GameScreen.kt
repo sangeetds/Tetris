31712f25e0c768560202b8fa0c539c31ec477b9f
sangeetnarayan <sangeetnarayands@gmail.com>
2020-09-17 14:00:56 +0530

    First Commit

---
+++ app/src/main/java/com/example/tetris/GameScreen.kt
@@ -0,0 +1,11 @@
+package com.example.tetris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GameScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_screen)
+    }
+}
