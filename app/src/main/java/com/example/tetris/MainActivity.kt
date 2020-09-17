3d2b2c255a0f1d1b07e44eb9b66b6e4a98b80524
sangeetnarayan <sangeetnarayands@gmail.com>
2020-09-17 17:04:26 +0530

    All Layouts added.

--- app/src/main/java/com/example/tetris/MainActivity.kt
+++ app/src/main/java/com/example/tetris/MainActivity.kt
@@ -1,11 +1,49 @@
 package com.example.tetris
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startGameButton = findViewById<Button>(R.id.startButton)
+        val scoreButton = findViewById<Button>(R.id.highScore)
+        val settingsButton = findViewById<Button>(R.id.settings)
+
+        startGameButton.setOnClickListener {
+            val mainGame = Intent(this, GameScreen::class.java)
+            startActivity(mainGame)
+        }
+
+        scoreButton.setOnClickListener {
+            val mainGame = Intent(this, GameScreen::class.java)
+            startActivity(mainGame)
+        }
+
+        startGameButton.setOnClickListener {
+            val mainGame = Intent(this, GameScreen::class.java)
+            startActivity(mainGame)
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.game_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val aboutIntent = Intent(this, About::class.java)
+        startActivity(aboutIntent)
+
+        return true
     }
 }
